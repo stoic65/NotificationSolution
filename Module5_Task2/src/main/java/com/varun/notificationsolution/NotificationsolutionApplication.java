278ab6a249b0ffe4d15c45bf3ad938124ce77b5e
Varun Ramasundaram <vramasundaram@expedia.com>
2019-04-25 07:18:22 +0530

    Initial commit

---
+++ Module5_Task2/src/main/java/com/varun/notificationsolution/NotificationsolutionApplication.java
@@ -0,0 +1,13 @@
+package com.varun.notificationsolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationsolutionApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NotificationsolutionApplication.class, args);
+  }
+
+}
